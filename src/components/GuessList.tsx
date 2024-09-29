420267fe42fbaed8e610f78896a4a769bb6c8a33
André Rosatto <linus1178@gmail.com>
2024-09-29 11:37:03 -0300

    added comments

--- src/components/GuessList.tsx
+++ src/components/GuessList.tsx
@@ -7,20 +7,22 @@ interface GuessListProps {
 
 export default function GuessList({guesses, country}: GuessListProps) {
 	return (
-		<div className="flex flex-col gap-1 items-center m-auto w-full max-w-sm">
+		<ul className="flex flex-col gap-1 items-center m-auto w-full max-w-sm">
 			{guesses.map(guess =>
-				<p key={guess} className="text-gray-200 w-full px-2 border border-gray-200 flex items-center justify-center uppercase">
+				<li key={guess} className="text-gray-200 w-full px-2 border border-gray-200 flex items-center justify-center uppercase">
 					{guess}
+
 					{/* wrong guess */}
 					{guess.toLowerCase() !== country.name.toLowerCase() && <svg className="size-5 ml-2 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
 					</svg>}
+
 					{/* right guess */}
 					{guess.toLowerCase() === country.name.toLowerCase() && <svg className="size-5 ml-2 text-green-600" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
 						<path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
 					</svg>}
-				</p>
+				</li>
 			)}
-		</div>
+		</ul>
 	);
 }
